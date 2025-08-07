79fa2c8c092dcd183ec8e35c85357910e062f14d
Bob Liu <akagi201@gmail.com>
2025-08-08 06:32:37 +0800

    chore(docs): remove footer text from documentation generation script

--- generate_docs.sh
+++ generate_docs.sh
@@ -16,8 +16,7 @@ uv run pdoc src/orderly_sdk \
   --search \
   --math \
   --no-include-undocumented \
-  --edit-url "orderly_sdk=https://github.com/longcipher/orderly-sdk-py/tree/master/src/orderly_sdk/" \
-  --footer-text "Orderly SDK for Python - <a href='https://github.com/longcipher/orderly-sdk-py'>GitHub</a> | <a href='https://pypi.org/project/orderly-sdk/'>PyPI</a> | <a href='https://orderly.network/docs/build-on-evm/building-on-evm'>Official API Docs</a>"
+  --edit-url "orderly_sdk=https://github.com/longcipher/orderly-sdk-py/tree/master/src/orderly_sdk/"
 
 echo "Documentation generated successfully in ./docs/"
 echo "Open ./docs/index.html in your browser to view the documentation."
